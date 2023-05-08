197daa89bc0f5e2bc0ce28740afa0ef8a9570632
zhongjiajia <zhongjiajia@360.cn>
2023-05-08 16:31:32 +0800

    Fixed SafeWriteAllData

--- fs/io.go
+++ fs/io.go
@@ -8,13 +8,15 @@ import (
 func SafeWriteAllData(w io.Writer, data []byte) (err error) {
 	for len(data) > 0 {
 		n, err := w.Write(data)
+		if n > 0 {
+			data = data[n:]
+		}
 		if err != nil {
 			if err == syscall.EAGAIN || err == syscall.EWOULDBLOCK {
 				continue
 			}
 			return err
 		}
-		data = data[n:]
 	}
 	return nil
 }
